0ced7c44ed8dbb15102783ccf9a268b160a7c2eb
unknown <pawankmaurya466@gmail.com>
2021-09-09 01:46:45 +0530

    crud_file

---
+++ crud/savedata.php
@@ -0,0 +1,12 @@
+<?php 
+echo $stu_name = $_POST['sname'];
+echo $stu_address = $_POST['saddress'];
+echo $stu_class = $_POST['class'];
+echo $stu_phone = $_POST['sphone'];
+
+$conn = mysqli_connect("localhost","root","","crud") or die("connection faild.");   
+$sql = "INSERT INTO student(sname,saddress,sclass,sphone) VALUES('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
+$result = mysqli_query($conn, $sql) or die(" Query unsussefully."); 
+header("Location: http://localhost/crud/index.php");
+mysqli_close($conn);
+?>
